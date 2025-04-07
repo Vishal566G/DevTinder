aab96a1d87da9186e42b58518c8a97a37c736c12
Vishal <visku199@gmail.com>
2025-04-07 20:45:25 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -3,27 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/user", (req, res) => {
+  console.log(req.query)
   res.send({ firstName: "Vishal", lastName: "Gaud" });
-});
-app.post("/user", (req, res) => {
-  // Data saved to db
-  res.send("Data saved to db");
-});
-
-app.put("/user", (req, res) => {
-  res.send("Data Updated to db, PUT call");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Data updated to db, PATCH call");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data deleted from db");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from dashboard! Test");
 });
 
 app.listen(3000, () => {
