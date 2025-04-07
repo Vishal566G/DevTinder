f775e17a0e5ded4d8074b5e9d874745824b35fd0
Vishal <visku199@gmail.com>
2025-04-07 20:16:14 +0530

    Exploring GET, PUT, POST, PATCH, DELETE

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from dashboard! Hola");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vishal", lastName: "Gaud" });
+});
+app.post("/user", (req, res) => {
+  // Data saved to db
+  res.send("Data saved to db");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello hello hello!");
+app.put("/user", (req, res) => {
+  res.send("Data Updated to db, PUT call");
 });
 
-app.get("/home", (req, res) => {
-  res.send("Hello from server home!");
+app.patch("/user", (req, res) => {
+  res.send("Data updated to db, PATCH call");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted from db");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from dashboard! Test");
 });
 
 app.listen(3000, () => {
