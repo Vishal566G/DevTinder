04fab1a97e35d3a2ffa20307e593c83bf979d534
Vishal <visku199@gmail.com>
2025-08-03 17:46:41 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -3,20 +3,48 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
+// Sign up API
 app.post("/signup", async (req, res) => {
   // Creating a new instance of User model
-  const user = new User({
-    firstName: "Sunny",
-    lastName: "shadow",
-    emailId: "vishal@gmail.com",
-    password: "vishal",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error saving user: " + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API - Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(400).send("Something went wrong");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
