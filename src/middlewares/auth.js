0eda2fd9ee65449aea0f0806f1b78d77e349ce15
Vishal <visku199@gmail.com>
2025-11-01 19:48:16 +0530

    Minor updates for UI app

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
     // finding token
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Invalid Token");
+      return res.status(401).send("Please Login!");
     }
 
     // verifying token & finding user
