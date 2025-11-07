8c08069ea5bfbc2ffc06ab59e497531e260467be
Vishal <visku199@gmail.com>
2025-11-07 19:51:22 +0530

    Updated /signup to send the signed up user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -23,8 +23,13 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password: passwordHash,
     });
-    await user.save();
-    res.json("User added successfully");
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    res.json({ message: "User added successfully", data: savedUser });
   } catch (err) {
     res.status(400).send("Error saving user: " + err.message);
   }
